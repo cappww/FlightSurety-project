247a026c1e9fddcde04489e4c538f349832a6f3b
Capp Wiedenhoefer <cappw1@gmail.com>
2019-06-29 16:29:39 -0400

    removed build folder

--- test/execute.js
+++ test/execute.js
@@ -2,7 +2,7 @@ module.exports = async (callback) => {
     const FlightSuretyApp = artifacts.require("FlightSuretyApp");
     let instance = await FlightSuretyApp.deployed();
     console.log(instance.address);
-
+    
     
     callback();
 }
