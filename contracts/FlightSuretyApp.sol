7b3968c97e8b8a1e97fc729d14878a6a8529bea3
Capp Wiedenhoefer <cappw1@gmail.com>
2019-07-03 13:48:42 -0400

    Created a separate contract to manage oracles

--- contracts/FlightSuretyApp.sol
+++ contracts/FlightSuretyApp.sol
@@ -1,37 +1,15 @@
 pragma solidity ^0.5.0;
 
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./OracleManager.sol";
 
 //FlightSurety Smart Contract
 
-contract FlightSuretyApp {
+contract FlightSuretyApp is OracleManager {
     using SafeMath for uint256;
 
     address payable private contractOwner;          // Account used to deploy contract
     FlightSuretyData flightSuretyData;      // Instance of the data contract
-    uint8 private nonce = 0;
-    uint256 public constant REGISTRATION_FEE = 1 ether;
-    uint256 private constant MIN_RESPONSES = 3;
-    struct Oracle {
-        bool isRegistered;
-        uint8[3] indexes;
-    }
-    mapping(address => Oracle) private oracles;
-
-    // Model for responses from oracles
-    struct ResponseInfo {
-        address requester;
-        bool isOpen;
-        mapping(uint8 => address[]) responses;
-    }
-    mapping(bytes32 => ResponseInfo) private oracleResponses;
-
-    //Events
-    event OracleRequest(uint8 index, address airline, uint flight, uint256 timestamp);
-    event OracleReport(address airline, uint flight, uint256 timestamp, uint8 status);
-    event FlightStatusInfo(address airline, uint flight, uint256 timestamp, uint8 status);
-
-    //MODIFIERS
 
     //CONSTRUCTOR
     constructor(address dataContract) public
@@ -39,88 +17,6 @@ contract FlightSuretyApp {
         contractOwner = msg.sender;
         flightSuretyData = FlightSuretyData(dataContract);
     }
-
-    function fetchFlightStatus
-    (
-        address airline,
-        uint flightNum,
-        uint timestamp
-    )
-        external returns(address)
-    {
-        uint8 index = getRandomIndex(msg.sender);
-        // Generate a unique key for storing the request
-        bytes32 key = keccak256(abi.encodePacked(index, airline, flightNum, timestamp));
-        oracleResponses[key] = ResponseInfo({
-            requester: msg.sender,
-            isOpen: true
-        });
-        emit OracleRequest(index, airline, flightNum, timestamp);
-        return msg.sender;
-    }
-
-    function submitOracleResponse
-    (
-        uint8 index,
-        address airline,
-        uint flight,
-        uint256 timestamp,
-        uint8 statusCode
-    )
-    external returns (uint)
-    {
-        // require(
-        //     (oracles[msg.sender].indexes[0] == index) ||
-        //     (oracles[msg.sender].indexes[1] == index) ||
-        //     (oracles[msg.sender].indexes[2] == index),
-        //     "Index does not match oracle request"
-        // );
-        bytes32 key = keccak256(abi.encodePacked(index, airline, flight, timestamp));
-        //require(oracleResponses[key].isOpen, "Flight or timestamp do not match oracle request");
-        oracleResponses[key].responses[statusCode].push(msg.sender);
-        // Information isn't considered verified until at least MIN_RESPONSES
-        // oracles respond with the *** same *** information
-        emit OracleReport(airline, flight, timestamp, statusCode);
-        if (oracleResponses[key].responses[statusCode].length >= MIN_RESPONSES) {
-            emit FlightStatusInfo(airline, flight, timestamp, statusCode);
-            // Handle flight status as appropriate
-            processFlightStatus(airline, flight, timestamp, statusCode);
-        }
-        return 500;
-    }
-
-    function processFlightStatus
-    (
-        address airline,
-        uint flight,
-        uint256 timestamp,
-        uint8 statusCode
-    )
-    internal pure
-    {
-        1+1;
-    }
-
-    function getRandomIndex
-    (
-        address account
-    )
-    public returns (uint8)
-    {
-        uint8 maxValue = 10;
-
-        // Pseudo random number...the incrementing nonce adds variation
-        uint8 random = uint8(uint256(keccak256(abi.encodePacked(blockhash(block.number - nonce++), account))) % maxValue);
-
-        if (nonce > 250) {
-            nonce = 0;  // Can only fetch blockhashes for last 256 blocks so we adapt
-        }
-
-        return random;
-    }
-
-    function() external payable {}
-
 }
 
 //Contract Data Interface
--- src/server/server.js
+++ src/server/server.js
@@ -1,55 +1,74 @@
-import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
-import Config from './config.json';
-import Web3 from 'web3';
-import express from 'express';
-
+const FlightSuretyApp = require('../../build/contracts/FlightSuretyApp.json');
+const Web3 = require('web3');
+const Config = require('./config.json');
+const express = require('express');
+const contract = require('truffle-contract');
+const truffleAssertions = require('truffle-assertions')
+const port = 3000;
 
 let config = Config['localhost'];
-let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
+let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url));
 web3.eth.defaultAccount = web3.eth.accounts[0];
-let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
-flightSuretyApp.events.OracleReport({
-    fromBlock: 0
-  }, async (err, event) => {
-    if (err) console.log(err)
-    else {
-      console.log("OracleResponse", event);
+let airline = '0x01839bE1cCA5D19F223Aa3eFD6794Ec4ddb02e18';
+let oracle = '0x5eB9112B0165a3d72490f6e2ACB82484035dED1C';
+
+class Server {
+    constructor() {
+        this.flightSuretyApp = web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+        this.app = express();
+        this.watchOracleReport();
+        this.watchOracleRequest();
+        this.startServer();
     }
-    
-  }
-);
 
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, async (error, event) => {
-    if (error) console.log(error)
-    else {
-      //console.log("event:", event)
-      let status = Math.floor(Math.random() * 6) * 10;
-      flightSuretyApp.methods.submitOracleResponse(
-        event.returnValues.index,
-        event.returnValues.airline,
-        event.returnValues.flight,
-        event.returnValues.timestamp,
-        event.returnValues.status,
-        {from: web3.eth.defaultAccount}
-      )
-
-      
-
-      //console.log("request received", event.returnValues.flight.toNumber());
+    watchOracleReport(){
+        this.flightSuretyApp.events.OracleReport(
+            { fromBlock: 0 },
+            async (err, ev) => {
+                if(err) console.log(err);
+                else {
+                    console.log(
+                        "OracleReport\n ",
+                        ev.returnValues.airline, "\n ",
+                        ev.returnValues.flight, "\n ",
+                        ev.returnValues.timestamp, "\n ",
+                        ev.returnValues.status,
+                    )
+                }
+            }
+        )
     }
-    //console.log("event:", event)
-});
 
-const app = express();
-app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
-})
+    watchOracleRequest(){
+        this.flightSuretyApp.events.OracleRequest(
+            { fromBlock: 0 }, 
+            async (err, ev) => {
+                if(err) console.log(err);
+                else {
+                    console.log(
+                        "OracleRequest\n ",
+                        ev.returnValues.index, "\n ",
+                        ev.returnValues.airline, "\n ",
+                        ev.returnValues.flight, "\n ",
+                        ev.returnValues.timestamp, "\n ",
+                    )
+                    let status = Math.floor(Math.random() * 6) * 10;
+                    await this.flightSuretyApp.methods.submitOracleResponse(
+                        ev.returnValues.index,
+                        ev.returnValues.airline,
+                        ev.returnValues.flight,
+                        ev.returnValues.timestamp,
+                        status
+                    ).send({from: oracle});
+                }
+            }
+        )
+    }
 
-export default app;
+    startServer() {
+        this.app.listen(port, () => console.log(`App listening on port ${port}`));
+    }
+}
 
-
+new Server();
